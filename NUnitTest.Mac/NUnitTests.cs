78eae017227cb52cc1ea4c774834b2a357a910d2
simeon052 <s>
2016-08-03 22:58:54 +0900

    Initial commit

---
+++ NUnitTest.Mac/NUnitTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using NUnitTest.PCL;
+namespace NUnitTest.Mac
+{
+	[TestFixture]
+	public class MyClass
+	{
+		public MyClass()
+		{
+		}
+
+		[Test]
+		public void Test1()
+		{
+			Assert.IsTrue(true);
+		}
+
+		[Test]
+		public void Test2()
+		{
+			var ans = MyPCLClass.Add(1, 1);
+			Assert.AreEqual(2, ans);
+		}
+	}
+}
+
